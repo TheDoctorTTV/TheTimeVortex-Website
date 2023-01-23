7ac113cfd5b506f8c36bd79cf91911931baf24e8
The Doctor <39390981+TheDoctor121027@users.noreply.github.com>
2023-01-23 14:38:28 -0500

    Header.js

--- assets/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <footer>
-        &copy; T h e D o c t o r 2022
-    </footer>
-</div>
--- assets/header.js
+++ assets/header.js
@@ -1,4 +1,10 @@
-<div class="Header">
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+  return (
+    <div class="Header">
     <h1>The Time Vortex</h1>
     <nav class="navbar">
       <ul class="navbar-links">
@@ -15,11 +21,9 @@
       </ul>
     </nav>
   </div>
-  <div class="mobile_nav">
-    <ul>
-      <li> <a href="index"> <img class="icons" src="icons/home-icon.svg" alt=""> </a></li>
-      <li> <a href="socials"> <img class="icons" src="icons/socials.svg" alt=""> </a></li>
-      <li> <a href="videos-hub"> <img class="icons" src="icons/tv-outline.svg" alt=""> </a> </li>
-      <li> <a href="minecraft-server"> <img class="icons" src="icons/minecraft.svg" alt=""> </a></li>
-    </ul>
-  </div>
+  );
+};
+
+
+export default Header;
+
