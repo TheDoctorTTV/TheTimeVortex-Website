a2604eb44a7c9a85ec581c58c37c1bba8bbf5357
The Doctor <39390981+TheDoctor121027@users.noreply.github.com>
2023-01-23 16:14:07 -0500

    Header.js

--- assets/header.js
+++ assets/header.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-
-const Header = () => {
-  return (
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
     <div class="Header">
-    <h1>The Time Vortex</h1>
-    <nav class="navbar">
-      <ul class="navbar-links">
-        <li> <a href="index"> Home </a></li>
-        <li> <a href="https://youtube.com/thedoctor121027" target="_blank"> Youtube </a></li>
-        <li> <a href="https://twitch.tv/ttv_thedoctor" target="_blank"> Twitch </a></li>
-        <li class="more-links"> <a href="#"> More </a>
-          <ul>
-            <li> <a href="https://twitter.com/TheDoctorTTV" target="_blank"> Twitter </a></li>
-            <li> <a href="videos-hub"> Stream VODs Hub </a></li>
-            <li> <a href="minecraft-server"> Minecraft Server </a></li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </div>
-  );
-};
+        <h1>The Time Vortex</h1>
+        <nav class="navbar">
+            <ul class="navbar-links">
+                <li> <a href="index"> Home </a></li>
+                <li> <a href="https://youtube.com/thedoctor121027" target="_blank"> Youtube </a></li>
+                <li> <a href="https://twitch.tv/ttv_thedoctor" target="_blank"> Twitch </a></li>
+                <li class="more-links"> <a href="#"> More </a>
+                    <ul>
+                        <li> <a href="https://twitter.com/TheDoctorTTV" target="_blank"> Twitter </a></li>
+                        <li> <a href="videos-hub"> Stream VODs Hub </a></li>
+                        <li> <a href="minecraft-server"> Minecraft Server </a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="mobile_nav">
+        <ul>
+            <li> <a href="index"> <img class="icons" src="icons/home-icon.svg" alt=""> </a></li>
+            <li> <a href="socials"> <img class="icons" src="icons/socials.svg" alt=""> </a></li>
+            <li> <a href="videos-hub"> <img class="icons" src="icons/tv-outline.svg" alt=""> </a> </li>
+            <li> <a href="minecraft-server"> <img class="icons" src="icons/minecraft.svg" alt=""> </a></li>
+        </ul>
+    </div>
+    `
+  }
+}
 
-
-export default Header;
-
+customElements.define('my-header', MyHeader)
