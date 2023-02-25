50085f549ec1b23b52433d9faf87068051b5b1f7
The Doctor <39390981+TheDoctor121027@users.noreply.github.com>
2023-02-24 22:43:59 -0500

    Minify HTML

--- assets/header.js
+++ assets/header.js
@@ -5,19 +5,19 @@ class MyHeader extends HTMLElement {
         <h1 class="main-header">The Time Vortex</h1>
         <nav class="navbar">
             <ul class="navbar-links">
-                <li> <a href="/index"> Home </a></li>
-                <li> <a href="/socials"> Socials </a></li>
-                <li> <a href="/videos-hub"> VODs Archive </a></li>
-                <li> <a href="/minecraft-server"> MC Server </a></li>               
+                <li> <a href="/index-min"> Home </a></li>
+                <li> <a href="/socials-min"> Socials </a></li>
+                <li> <a href="/videos-hub-min"> VODs Archive </a></li>
+                <li> <a href="/minecraft-server-min"> MC Server </a></li>               
             </ul>
         </nav>
     </div>
     <div class="mobile_nav">
         <ul>
-            <li> <a href="/index"> <img class="icons" src="/icons/home-icon.svg" alt=""> </a></li>
-            <li> <a href="/socials"> <img class="icons" src="/icons/socials.svg" alt=""> </a></li>
-            <li> <a href="/videos-hub"> <img class="icons" src="/icons/tv-outline.svg" alt=""> </a> </li>
-            <li> <a href="/minecraft-server"> <img class="icons" src="/icons/minecraft.svg" alt=""> </a></li>
+            <li> <a href="/index-min"> <img class="icons" src="/icons/home-icon.svg" alt=""> </a></li>
+            <li> <a href="/socials-min"> <img class="icons" src="/icons/socials.svg" alt=""> </a></li>
+            <li> <a href="/videos-hub-min"> <img class="icons" src="/icons/tv-outline.svg" alt=""> </a> </li>
+            <li> <a href="/minecraft-server-min"> <img class="icons" src="/icons/minecraft.svg" alt=""> </a></li>
         </ul>
     </div>
     `
